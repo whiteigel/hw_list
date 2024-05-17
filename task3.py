cb48ee35e1ae660aa847d84811a594cc50b43ca9
Whiteigel <whiteigel@gmail.com>
2024-05-17 21:07:13 +0200

    Minor change

--- task3.py
+++ task3.py
@@ -1,14 +1,16 @@
 num = 1
+limit = 100
 even_list = []
-while num <= 100:
+while num <= limit:
     if num % 2 == 0:
         even_list.append(num)
     num += 1
 print(even_list)
 
 num = 1
+limit = 100
 even_list = []
-while num <= 100:
+while num <= limit:
     if num % 2 == 0:
         sqr = num ** 2
         even_list.append(sqr)
