82bc815b24f807c479fe9994854601cac53611da
Whiteigel <whiteigel@gmail.com>
2024-05-18 06:41:24 +0200

    Logic changed

--- task3.py
+++ task3.py
@@ -1,18 +1,15 @@
-num = 1
-limit = 100
+start_num = int(input("Enter a number to start with: "))
+end_num = int(input("Enter a number to end with: "))
+
 even_list = []
-while num <= limit:
-    if num % 2 == 0:
-        even_list.append(num)
-    num += 1
+while start_num <= end_num:
+    if start_num % 2 == 0:
+        even_list.append(start_num)
+    start_num += 1
 print(even_list)
 
-num = 1
-limit = 100
-even_list = []
-while num <= limit:
-    if num % 2 == 0:
-        sqr = num ** 2
-        even_list.append(sqr)
-    num += 1
+counter = 0
+while counter < len(even_list):
+    even_list[counter] = even_list[counter] ** 2
+    counter += 1
 print(even_list)
